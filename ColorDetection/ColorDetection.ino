8b691ca44e6d91c157b6ca38ab6490ce24bdf0a4
elmariofredo <mario@vejlupek.cz>
2017-03-12 13:32:23 +0100

    Initial version of color detection

--- ColorDetection/ColorDetection.ino
+++ ColorDetection/ColorDetection.ino
@@ -1,110 +1,45 @@
-// Color Detector
-
-// Setup pins
-#define pinS0 4
-#define pinS1 5
-#define pinS2 6
-#define pinS3 7
-#define pinOut 8
-
-void setup() {
-
-  // Setup output pins
-  pinMode(pinS0, OUTPUT);
-  pinMode(pinS1, OUTPUT);
-  pinMode(pinS2, OUTPUT);
-  pinMode(pinS3, OUTPUT);
-
-  // Setup input pin
-  pinMode(pinOut, INPUT);
-
-  // Init color detector module
-  digitalWrite(pinS0,HIGH);
-  digitalWrite(pinS1,LOW);
-
-  // Start serial line
-  Serial.begin(9600);
-
+/*
+Adafruit Arduino - Lesson 3. RGB LED
+*/
+ 
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+ 
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
 }
-void loop() {
-
-  // Color frequences
-  int freqRed, freqGreen, freqBlue;
-
-  //// Red
-
-  // Setup Red color detection
-  digitalWrite(pinS2,LOW);
-  digitalWrite(pinS3,LOW);
-
-  // Delay for precise measurement
-  delay(50);
-
-  // Read Red freq value
-  freqRed = pulseIn(pinOut, LOW);
-
-  /// Green
-
-  // Setup Green color detection
-  digitalWrite(pinS2,HIGH);
-  digitalWrite(pinS3,HIGH);
-
-  // Delay for precise measurement
-  delay(50);
-
-  // Read Green freq value
-  freqGreen = pulseIn(pinOut, LOW);
-
-  //// Blue
-
-  // Setup Blue color detection
-  digitalWrite(pinS2,LOW);
-  digitalWrite(pinS3,HIGH);
-
-  // Delay for precise measurement
-  delay(50);
-
-  // Read Blue freq value
-  freqBlue = pulseIn(pinOut, LOW);
+ 
+void loop()
+{
   
-  // Print frequences over serial line
-  Serial.print("R: ");
-  Serial.print(freqRed);
-  Serial.print(" | G: ");
-  Serial.print(freqGreen);
-  Serial.print(" | B: ");
-  Serial.print(freqBlue);
+  setColor(0, 255, 0);  // Red
+  delay(2000);
 
-  // Detect exact color and print to serial line 
-  if (freqRed < 60) {
-    Serial.print(" | RED ");
-        pinMode(10, OUTPUT);
-    digitalWrite(10, HIGH);
-  }
+  setColor(0, 0, 255);  // Green
+  delay(2000);
 
-  if (freqGreen < 60 ) {
-    Serial.print(" | GREEN ");
-    pinMode(9, OUTPUT);
-    digitalWrite(9, HIGH);
-  }
+  setColor(255, 0, 0);  // Blue
+  delay(2000);
+
   
-  if (freqBlue < 60) {
-    Serial.print(" | BLUE ");
-  }
-
-  // Custom color detection
-  if (freqRed < 60 & freqGreen < 60 ) {
-    Serial.print(" | Detekce zlute. ");
-  }
-
-  // Serial new line
-  Serial.println();
-
-  // Delay for reading results
-  delay(850);
-
-  // Disable LEDs
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-
+//  delay(2000);
+//  setColor(0, 0, 255);  // blue
+//  delay(2000);
+//  setColor(255, 255, 0);  // yellow
+//  delay(2000);  
+//  setColor(80, 0, 80);  // purple
+//  delay(2000);
+//  setColor(0, 255, 255);  // aqua
+//  delay(2000);
+}
+ 
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
 }
