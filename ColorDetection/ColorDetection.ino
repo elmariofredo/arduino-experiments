829646bbed75fe94795869f6b24536470fff1b79
elmariofredo <mario@vejlupek.cz>
2017-03-12 13:33:12 +0100

    Initial version of color detection

--- ColorDetection/ColorDetection.ino
+++ ColorDetection/ColorDetection.ino
@@ -1,7 +1,4 @@
-/*
-Adafruit Arduino - Lesson 3. RGB LED
-*/
- 
+
 int redPin = 11;
 int greenPin = 10;
 int bluePin = 9;
@@ -25,16 +22,6 @@ void loop()
   setColor(255, 0, 0);  // Blue
   delay(2000);
 
-  
-//  delay(2000);
-//  setColor(0, 0, 255);  // blue
-//  delay(2000);
-//  setColor(255, 255, 0);  // yellow
-//  delay(2000);  
-//  setColor(80, 0, 80);  // purple
-//  delay(2000);
-//  setColor(0, 255, 255);  // aqua
-//  delay(2000);
 }
  
 void setColor(int red, int green, int blue)
